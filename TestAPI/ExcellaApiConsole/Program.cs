250ff41d7246e80fae9eed9c412de328de71f4fd
Parijat Sinha <parijat.sinha@outlook.com>
2016-01-30 12:30:51 -0500

    All

---
+++ TestAPI/ExcellaApiConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcellaApiConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new object[2];
+            obj[0] = new string[] { "one", "new" };
+            obj[1] = new string[] { "not", "net" };
+            var ret = PostIsAnagram(obj);
+            Console.WriteLine(IsAnagram("parijat", "tajripa"));
+            Console.ReadKey();
+        }
+
+        public static object PostIsAnagram(object[] value)
+        {
+            var retarray = new bool[value.Length];
+            try
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    var arr = value[i] as string[];
+                    retarray[i] = IsAnagram(arr[0], arr[1]);
+                }
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+            return retarray;
+        }
+
+        public static bool IsAnagram(string a, string b)
+        {
+            var a_chars = a.ToArray();
+            Array.Sort(a_chars);
+            var b_chars = b.ToArray();
+            Array.Sort(b_chars);
+            if (a_chars.Length != b_chars.Length) return false;
+            bool ret = true;
+            for (int i = 0; i < a_chars.Length; i++)
+            {
+                if (a_chars[i] != b_chars[i])
+                {
+                    ret = false;
+                    break;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
--- TestAPI/TestAPI/Methods/Methods.cs
+++ TestAPI/TestAPI/Methods/Methods.cs
@@ -44,5 +44,110 @@ namespace TestAPI.Methods
 
             return ret;
         }
+
+        public static int SumSquares(int a)
+        {
+            int sum = 0;
+            var arr = a.ToString().ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var val = int.Parse(arr[i].ToString());
+                sum += (int)Math.Pow(val, 2);
+            }
+
+            return sum;   
+        }
+
+        public static bool IsPrime(int a)
+        {
+            bool retval = true;
+            if (a <= 1)
+                retval = false;
+            else if (a <= 3)
+                retval = true;
+            else if (a % 2 == 0 || a % 3 == 0)
+                retval = false;
+            else
+            {
+                int index = 5;
+                while(index*index <= a)
+                {
+                    if (a % index == 0 | a % (index + 2) == 0)
+                        retval = false;
+                    index += 6;
+                }
+            }
+
+            return retval;
+        }
+
+        public static int GetFib(int index)
+        {
+            if (index == 1 || index == 2) return 1;
+            int a = 1;
+            int b = 1;
+            int fib = 0;
+            for(int i= 3; i<= index; i++)
+            {
+                fib = a + b;
+                a = b;
+                b = fib;
+            }
+            return fib;
+
+        }
+
+
+        public static List<int> SexyPrimes(int max)
+        {
+            var ret = new List<int>();
+            for(int i = 2; i <= max - 6; i++)
+            {
+                if (IsPrime(i) && IsPrime(i + 6))
+                    ret.Add(i);
+
+            }
+
+            return ret;
+        }
+
+        public static List<List<int>> PyTriplets(int max)
+        {
+            var lst = new List<List<int>>();
+            for(int i=1;i<=max;i++)
+            {
+                for(int j = 1; j <= max; j++)
+                {
+                    int sumsquare = i * i + j * j;
+                    double k = Math.Sqrt(sumsquare);
+                    if(k % 1 == 0 && k <= max && i < j && (i + j) > k )
+                    {
+                        lst.Add(new List<int>() { i, j, (int)k });
+                    }
+                }
+            }
+            lst.Sort((a, b) => a.ElementAt(2).CompareTo(b.ElementAt(2)));
+            return lst;
+        }
+
+        static List<List<int>> board = new List<List<int>>()
+        {
+            new List<int>() {0,0,0,0,0,0,0 },
+            new List<int>() {0,0,1,1,1,0,0 },
+            new List<int>() {0,1,0,0,1,0,0 },
+            new List<int>() {0,0,0,0,0,0,0 },
+            new List<int>() {0,0,0,0,0,0,0 },
+            new List<int>() {0,0,0,0,0,0,0 },
+            new List<int>() {0,0,0,0,0,0,0 },
+        };
+
+        static void playGameoflife()
+        {
+
+        }
+
+
+       
+        
     }
 }
